ef91e404afdab61a19c4b1bdf4be7299db78b2ae
Sheldon Bean <sheldon.bean@gmail.com>
2019-09-07 22:15:23 -0600

    Added OnDestroy UnityEvent to ClickDestroy script 9/7/2019

--- Experimenting with Sutff (1620)/Assets/Scripts/ClickDestroy.cs
+++ Experimenting with Sutff (1620)/Assets/Scripts/ClickDestroy.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ClickDestroy : MonoBehaviour
 {
+    public UnityEvent Vernichtung;
     private void OnMouseDown()
     {
         Destroy(gameObject);
-            
+    }
+
+    private void OnDestroy()
+    {
+        Vernichtung.Invoke();
     }
 
     // Start is called before the first frame update
