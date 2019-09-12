1d9cf5786ba68176ebd1f00e6340ef700202a03f
Sheldon <sheldon.bean@gmail.com>
2019-09-12 15:49:00 -0600

    learned about Cats and Dogs and mousedowns 9/12/2019

---
+++ CreateProjectPractice/Assets/Scripts/CatClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CatClass : MonoBehaviour
+{
+ public UnityEvent catStuff;
+
+ private void OnMouseDown()
+ {
+  catStuff.Invoke();
+ }
+ 
+}
---
+++ CreateProjectPractice/Assets/Scripts/ParticleControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+
+    public ParticleSystem particles;
+
+    void Start()
+    {
+        particles.Emit(100);
+    }
+
+    private void OnMouseDown()
+    {
+        particles.Emit(100);
+    }
+}
