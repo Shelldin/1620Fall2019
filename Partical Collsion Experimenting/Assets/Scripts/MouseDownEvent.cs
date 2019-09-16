c7ca89f89014738224f219732e13b24086adb7a9
Sheldon Bean <sheldon.bean@gmail.com>
2019-09-15 23:52:41 -0600

    Created new project for figuring out Particle collision 9/15/2019

---
+++ Partical Collsion Experimenting/Assets/Scripts/MouseDownEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseDownEvent : MonoBehaviour
+{
+   public UnityEvent mouseDown;
+
+   private void OnMouseDown()
+   {
+      mouseDown.Invoke();
+   }
+}
