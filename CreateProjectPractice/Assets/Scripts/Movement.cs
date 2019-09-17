a5c361200add84cccbf738f112ba68751e5fae0d
Sheldon <sheldon.bean@gmail.com>
2019-09-17 15:59:31 -0600

    Learned Player Controllers 9/17/2019

---
+++ CreateProjectPractice/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float gravity = -3f;
+    public float speed = 1f;
+    public float jumpSpeed = 10f;
+
+    private int jumpCount = 2;
+
+    private Vector3 position;
+    public CharacterController controller;
+
+   
+    void Update()
+    {
+        position.x = speed*Input.GetAxis("Horizontal");
+        position.y -= gravity;
+
+        if (!controller.isGrounded )
+        {
+
+           
+            position.y -= gravity;
+        }
+        else
+        {
+            jumpCount = 2;
+        }
+        
+        
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            
+            jumpCount--;
+            position.y = jumpSpeed;
+        }
+        controller.Move(position*Time.deltaTime);
+        
+    }
+}
---
+++ Experimenting with Sutff (1620)/Assets/Scripts/ParColEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ParColEvent : MonoBehaviour
+{
+
+    public UnityEvent parCol;
+   
+
+    private void OnParticleCollision(GameObject other)
+    {
+        parCol.Invoke();
+    }
+}
