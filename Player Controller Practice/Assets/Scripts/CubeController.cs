c3f50c037402130037f7db4d50fdc17a5ee247a1
Sheldon Bean <sheldon.bean@gmail.com>
2019-09-19 14:05:50 -0600

    Added cube controller to practice writing controller script 9/19/19

---
+++ Player Controller Practice/Assets/Scripts/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float gravity = 3f;
+    public float jumpSpeed = 25f;
+
+    private int jumpCount = 1;
+
+    public Vector3 position;
+    public CharacterController controller;
+    void Update()
+    {
+        controller.Move(position * Time.deltaTime);
+        position.x = speed * Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+
+        else
+        {
+            jumpCount = 1;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount >0)
+        {
+            jumpCount--;
+            position.y = jumpSpeed;
+        }
+    }
+}
