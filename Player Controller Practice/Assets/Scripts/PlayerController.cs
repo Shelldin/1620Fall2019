61f9cb05f2b4bd3045c7e7bcd616f5b2d7d280da
Sheldon Bean <sheldon.bean@gmail.com>
2019-09-18 23:55:09 -0600

    Practiced making character controller script 9/18/2019

---
+++ Player Controller Practice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 1f;
+    public float gravity = 3f;
+    public float jumpSpeed = 10f;
+
+    private int jumpCount = 1;
+    
+    private Vector3 position;
+    public CharacterController controller;
+    void Update()
+    {
+        position.x = speed*Input.GetAxis("Horizontal");
+        
+
+        if (!controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+
+        else
+        {
+            jumpCount = 1;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+        {
+            jumpCount--;
+            position.y = jumpSpeed;
+        }
+        
+        controller.Move(position * Time.deltaTime);
+    }
+}
