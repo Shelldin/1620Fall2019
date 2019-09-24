8ea9bab4a277273ec30c7d741ce92016a940cce9
Sheldon <sheldon.bean@gmail.com>
2019-09-24 15:52:10 -0600

    Learned If Statements and Rigidbodies 9/24/2019

---
+++ CreateProjectPractice/Assets/Scripts/IfElseFruit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElseFruit : MonoBehaviour
+{
+    public bool appleIsRipe, 
+        orangeIsRipe, 
+        peachIsRipe;
+
+    void Start()
+    {
+        if (appleIsRipe)
+        {
+            print("Go pick the apple.");
+        }
+        else if (orangeIsRipe)
+        {
+            print("Go pick the orange.");
+            
+        }
+        else if (peachIsRipe)
+        {
+             print("Go pick the peach.");
+             
+        }
+        else
+        {
+            print("Nothing is ripe");
+        }
+    }
+
+}
---
+++ CreateProjectPractice/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+    public int a = 5,
+        b = 5,
+        c = 10;
+
+    public string password = "OU812";
+    public bool lightsOn = true;
+    void Start()
+    {
+        if (a+b == c)
+        {
+            print(true);
+        }
+        else
+        {
+            print(false);
+        }
+
+        if (password == "OU812")
+        {
+            print(true);
+        }
+
+        if (lightsOn)
+        {
+            print(true);
+        }
+    }
+
+}
