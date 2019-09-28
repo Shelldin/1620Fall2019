c7114a418c741c22e6da4a002f63e1b6c89819a0
Sheldon Bean <sheldon.bean@gmail.com>
2019-09-27 23:42:13 -0600

    Created Assignment 2 9/27/2019

---
+++ Assignment 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 position;
+    private int jumpCount = 0;
+
+    public int jumpCountMax = 2;
+
+    public float moveSpeed = 10f,
+        gravity = 3f,
+        jumpSpeed = 30f;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+
+    void Update()
+    {
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+        controller.Move(position * Time.deltaTime);
+        
+        if(!controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+        else
+        {
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            jumpCount++;
+            position.y = jumpSpeed;
+        }
+    }
+}
