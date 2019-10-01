157dff5aae40120cc686a35f4ff2e1c2c9b093bf
Sheldon Bean <sheldon.bean@gmail.com>
2019-10-01 13:07:55 -0600

    Added to PlayerData 10/1/2019

---
+++ CreateProjectPractice/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public float health = 1f,
+        moveSpeed = 10f,
+        jumpSpeed = 30f,
+        attackPower = 10f,
+        defence = 1f;
+    
+    
+
+
+
+}
