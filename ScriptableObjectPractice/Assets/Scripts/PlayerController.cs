a340335988d5cc22e6e8cfebf6cec703bc3d3808
Sheldon Bean <sheldon.bean@gmail.com>
2019-10-03 01:27:00 -0600

    10/3/2019

---
+++ ScriptableObjectPractice/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>()
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ ScriptableObjectPractice/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    public string playerName;
+
+    public int jumpCount = 0,
+        jumpCountMax = 1;
+
+    public float moveSpeed = 10f,
+        gravity = 3f,
+        jumpSpeed = 30f;
+}
+
