4bec07e0614bed35c44aac8bcf563ec2b007adc7
Sheldon Bean <sheldon.bean@gmail.com>
2019-10-04 23:10:56 -0600

    Created CharacterController Script using ScriptableObject Data 10/4/2019

---
+++ ScriptableObjectPractice/Assets/Scripts/CharacterData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+[CreateAssetMenu]
+public class CharacterData : ScriptableObject
+{
+    public string characterName;
+
+    public int jumpCount = 0,
+        jumpCountMax = 1;
+
+    public float moveSpeed = 10f;
+
+    public float gravity = 3f,
+        jumpSpeed = 30f;
+
+    public void UpdateJumpMax(int amount)
+    {
+        jumpCountMax += amount;
+    }
+
+    public void UpdateMoveSpeed(float amount)
+    {
+        moveSpeed += amount;
+    }
+
+    public void UpdateGravity(float amount)
+    {
+        gravity += amount;
+    }
+
+    public void UpdateJumpSpeed(float amount)
+    {
+        jumpSpeed += amount;
+    }
+}
+
--- ScriptableObjectPractice/Assets/Scripts/PlayerController.cs
+++ ScriptableObjectPractice/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
+    private Vector3 position;
+
+    public CharacterData playerData;
+    
     
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        //playerData = GetComponent<CharacterData>(); not sure if I need this or how to get a specific CharacterData
     }
 
-    void Update()
+
+    private void Update()
     {
-        
+        controller.Move(position * Time.deltaTime);
+        position.x = playerData.moveSpeed * Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded)
+        {
+            position.y -= playerData.gravity;
+        }
+        else
+        {
+            playerData.jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && playerData.jumpCount < playerData.jumpCountMax)
+        {
+            playerData.jumpCount++;
+            position.y = playerData.jumpSpeed;
+        }
     }
 }
