b7a11eefea2adeb8591307cd742b3197bb99c050
Sheldon <sheldon.bean@gmail.com>
2019-10-22 15:57:28 -0600

    lists 10/22/2019

---
+++ CreateProjectPractice/Assets/Scripts/DataScripts/GameArtCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+
+public class GameArtCollection : ScriptableObject
+
+{
+    public List<GameArtData> collectionList;
+
+    public void AddData(GameArtData obj)
+    {
+        if(!obj.collected)
+        {
+            collectionList.Add(obj);
+            obj.collected = true;
+        }
+    }
+}
--- CreateProjectPractice/Assets/Scripts/TriggerEvents.cs
+++ CreateProjectPractice/Assets/Scripts/TriggerEvents.cs
@@ -2,10 +2,16 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
-
+[RequireComponent(typeof(Collider))]
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEvent;
+
+    private void Awake()
+    {
+      GetComponent<Collider>().isTrigger = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     { 
         triggerEvent.Invoke();
