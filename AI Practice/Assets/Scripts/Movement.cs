6b4993a828fde63693743faab77b83ceb2f0d972
Sheldon Bean <sheldon.bean@gmail.com>
2019-11-04 23:56:14 -0700

    Practiced Making AI

---
+++ AI Practice/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 position;
+
+    public float moveSpeed = 10f;
+    
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+   
+    void Update()
+    {
+        controller.Move(position * Time.deltaTime);
+        position.x = moveSpeed * Input.GetAxis("Horizontal");
+    }
+}
