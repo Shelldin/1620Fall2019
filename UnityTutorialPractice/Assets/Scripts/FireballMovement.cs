833799bea98f551b1739fe9e1f3b722c8c75fea5
Sheldon Bean <sheldon.bean@gmail.com>
2019-11-09 23:43:51 -0700

    Trying to create a projectile system 11/9/2019

--- UnityTutorialPractice/Assets/Scripts/FireballMovement.cs
+++ UnityTutorialPractice/Assets/Scripts/FireballMovement.cs
@@ -8,10 +8,18 @@ public class FireballMovement : MonoBehaviour
     private CharacterController controller;
     private Vector3 position;
     public float moveSpeed = 15f;
-
+    public GameObject prefab;
+    
     private void Start()
     {
+
+        position = prefab.transform.position; 
         controller = GetComponent<CharacterController>();
+    }
+
+    private void Awake()
+    {
+        position = prefab.transform.position;
     }
 
     void Update()
@@ -20,3 +28,4 @@ public class FireballMovement : MonoBehaviour
         position.x = moveSpeed;
     }
 }
+
---
+++ UnityTutorialPractice/Assets/Scripts/KeyUpShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyUpShoot : MonoBehaviour
+{
+
+    private GameObject bullet;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            bullet = new GameObject();
+        }
+    }
+    
+}
