9b0052e7102a698709c8d5f79752f2de56049775
Sheldon <sheldon.bean@gmail.com>
2019-11-12 15:52:39 -0700

    coroutines 11/12/2019

---
+++ CreateProjectPractice/Assets/Scripts/CoRoutines/Coroutines.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+
+    public UnityEvent startEvent, repeatEvent, endEvent;
+
+    public float seconds = 1f;
+    public bool canRun;
+
+    public int counter = 10;
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        endEvent.Invoke();
+    }
+
+
+
+
+
+}
---
+++ CreateProjectPractice/Assets/Scripts/CoRoutines/CoroutinesExample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CoroutinesExample : MonoBehaviour
+{
+
+    public float seconds = 1f;
+    
+    private int i = 3;
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    IEnumerator Start()
+    {
+        while (i>0)
+        {
+            yield return wfsObj;
+                    Debug.Log(i);
+                    i--;
+        }
+        yield return new WaitForSeconds(1f);
+        Debug.Log("GO!");
+        
+        // additional notes
+        // yield return new WaitForSeconds(1f);
+       // Debug.Log(3);
+       // Invoke(nameof(StartLater), 3f);
+
+    }
+
+    void StartLater()
+    {
+        Debug.Log("ran");
+    }
+
+ 
+
+
+    
+}
