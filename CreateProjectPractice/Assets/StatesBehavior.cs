7edd0dce3d3d0704715f4b4859b2c72be9e515d0
Sheldon <sheldon.bean@gmail.com>
2019-11-21 15:53:29 -0700

    State Machine and Game Action 11/21/2019

---
+++ CreateProjectPractice/Assets/StatesBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StatesBehavior : StateMachineBehaviour
+{
+
+    public UnityEvent startEnterEvent, stateUpdateEvent, stateExitEvent;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        startEnterEvent.Invoke();
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateUpdateEvent.Invoke();
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        stateExitEvent.Invoke();
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
